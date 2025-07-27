257ffa14f4ddb8136378362b9ea8d6be6e46eab0
Eduardo <46777616+eddiesanch8@users.noreply.github.com>
2025-07-27 09:33:21 -0400

    finalized JS logic, added delete, and fixed bug

--- index.js
+++ index.js
@@ -1,66 +1,77 @@
 `use strict`;
 
 // Core Layer
+let shouldResetScreen = false;
 
-// const operatorList = ["+", "-", "=", "×", "÷"];
-
-// this code is being used to display hidden message
-// let totalDisplay =
-//   firstOperand + " " + "+" + ` ${secondOperand.toString()}` + " = ";
-// prevOutput.innerText = totalDisplay;
+// Function that takes two string arguements, our previous display and our current display
 
 function calculateOperation(str1, str2) {
+  // 4 "if" statements look for a specific operator.
   if (str1.includes(`+`)) {
+    let operator = "+";
+    // once we find the operator, we have to seperate the string into its number and away from its operator.
     let firstOperand = str1.slice(0, str1.indexOf(`+`));
+    // we also turn the current input from a string into a number
+
     let secondOperand = Number(str2);
+
+    // finally, we add the two numbers together
     let total = Number(firstOperand) + secondOperand;
+    // we clear everything so that it appears to the user as if a calculation has gone through.
+
     allClear();
+    // Then, our total is turned into a string that is displayed onto our display
     currentOutput.innerText = total.toString();
-    console.log(`You can add`);
+    // This sets up the logic to prevent the user from appending more numbers to result
+
+    shouldResetScreen = true;
+    // displays the history so that the user knows their calculation went through
+
+    getHistory(firstOperand, secondOperand, operator);
   }
 
   if (str1.includes("-")) {
+    let operator = "-";
     let firstOperand = str1.slice(0, str1.indexOf(`-`));
     let secondOperand = Number(str2);
     let total = Number(firstOperand) - secondOperand;
     allClear();
     currentOutput.innerText = total.toString();
-    console.log("You Subtracted");
+    shouldResetScreen = true;
+    getHistory(firstOperand, secondOperand, operator);
   }
 
   if (str1.includes(`×`)) {
+    let operator = "×";
     let firstOperand = str1.slice(0, str1.indexOf(`×`));
     let secondOperand = Number(str2);
-    let total = Number(firstOperand) + secondOperand;
+    let firstTotal = Number(firstOperand) * secondOperand;
+    let total = firstTotal.toFixed(2);
     allClear();
     currentOutput.innerText = total.toString();
-    console.log("You multiplied");
+    shouldResetScreen = true;
+    getHistory(firstOperand, secondOperand, operator);
   }
+
   // && str2[0] !== 0
-  if (str1.includes("÷") && !str2.startsWith("0")) {
+  if (str1.includes("÷") && str2 !== "0") {
+    let operator = "÷";
     let firstOperand = str1.slice(0, str1.indexOf(`÷`));
     let secondOperand = Number(str2);
-    let total = Number(firstOperand) / secondOperand;
+    let firstTotal = Number(firstOperand) / secondOperand;
+    let total = firstTotal.toFixed(2);
     allClear();
     currentOutput.innerText = total.toString();
-    console.log("You divided");
-  } else if (str2.startsWith("0")) {
+    shouldResetScreen = true;
+    getHistory(firstOperand, secondOperand, operator);
+  } else if (str2 === "0") {
     allClear();
+    shouldResetScreen = true;
     currentOutput.innerHTML = "NaN !";
   }
 }
 
-// Core Functionality with Joe
-
 // DOM Layer - 2
-
-// handle this as the calculation
-let currentHistory = [];
-
-// displays calculation
-let currentDisplay = "";
-
-let numberHistory = ["number1", "operator", "="];
 
 //1.  I need a way to declare all my vars for now
 const numberBtn = document.querySelectorAll(`.calc__btn--num`);
@@ -70,6 +81,7 @@ const allClearBtn = document.querySelector(`.calc__btn--clear`);
 const deleteBtn = document.querySelector(`.calc__btn--delete`);
 const prevOutput = document.querySelector(`.previous`);
 const currentOutput = document.querySelector(`.current`);
+const calcHistory = document.querySelector("[data-history]");
 
 // Used to Clear Previous and Current Output
 
@@ -77,14 +89,34 @@ function allClear() {
   // sets the inner text to an empty string effectively "clearing" it
   currentOutput.innerText = "";
   prevOutput.innerText = "";
+  calcHistory.innerText = "";
+}
+
+///////
+
+function deleteInput() {
+  if (prevOutput.innerText !== "") {
+    let deletedPrevString = prevOutput.innerText.slice(0, -1);
+    prevOutput.innerText = deletedPrevString;
+  } else if (currentOutput.innerText !== "") {
+    let deletedString = currentOutput.innerText.slice(0, -1);
+    currentOutput.innerText = deletedString;
+  } else if (calcHistory.innerText !== "") {
+    allClear();
+  }
 }
 
 // Updates our display depending on the value that the individual buttons give us
 // It might be helpful to look at the eventListeners to see where we are calling these
 // functions.
 function updateDisplay(userButton) {
-  // continuously adds the input from our calculator button to the display unless we already have a period
+  if (shouldResetScreen) {
+    currentOutput.innerText = "";
+    shouldResetScreen = false;
+  }
+
   if (!currentOutput.innerText.includes(".")) {
+    // continuously adds the input from our calculator button to the display unless we already have a period
     // appends any number pressed but only allows for 1 period
     currentOutput.innerText += userButton;
   } else if (currentOutput.innerText.includes(`.`) && userButton !== `.`) {
@@ -92,7 +124,7 @@ function updateDisplay(userButton) {
   }
 }
 
-// A second update display functioned is used to stop a user from
+// A second update display function is used to stop a user from
 // adding multiple operators to the field
 
 function updateDisplay2(userButton) {
@@ -124,6 +156,18 @@ function updateDisplay2(userButton) {
   }
 }
 
+function getHistory(firstStr, secondNumber, operatorStr) {
+  let totalDisplay =
+    firstStr + " " + `${operatorStr}` + ` ${secondNumber.toString()}` + " = ";
+  calcHistory.innerText = totalDisplay;
+  hideHistory(totalDisplay);
+}
+
+function hideHistory(totalStr) {
+  let hiddenText = totalStr + currentOutput.innerText;
+  console.log(hiddenText, currentOutput.innerText);
+}
+
 // DOM Layer
 
 numberBtn.forEach((btn) => {
@@ -140,18 +184,16 @@ operationBtn.forEach((btn) => {
 
     // end of Event Listner
   });
-
-  // End of .forEach function
 });
 
 equalBtn.addEventListener(`click`, () => {
-  console.log(prevOutput.innerText);
-
   calculateOperation(prevOutput.innerText, currentOutput.innerText);
 });
 
 allClearBtn.addEventListener(`click`, () => {
-  // Sets all strings to empty on display
-
   allClear();
+});
+
+deleteBtn.addEventListener("click", () => {
+  deleteInput();
 });
